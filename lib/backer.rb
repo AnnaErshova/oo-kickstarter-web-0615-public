1b22d16cd7f488b41385cabdcc9c57c000c15429
AnnaErshova <anna.ershova@gmail.com>
2015-06-15 17:34:08 -0400

    adds lecture notes

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,17 +1,21 @@
 class Backer
 
-attr_accessor :name, :backed_projects
+  attr_reader :name, :backed_projects # otherwise we get an undefined method error
 
-def initialize(name) # takes a name on initialization
-  @name = name
-  @backed_projects = Array.new # initializes empty array of projects which needs to be filled with backers from class Project
-end
+# a backer is initialzied with a name and an empty project array
+  def initialize(name) # takes a name on initialization
+    @name = name # saves it as an attribute
+    @backed_projects = Array.new # initializes a placeholder empty array of projects which needs to be filled with backers from class Project
+  # expect(spencer.backed_projects).to include(magic) - this is how we know it is an instance variable
+  end
 
-def back_project(project)
-
-  @backed_projects << project
-  project.backers << self
-  
-end
+  def back_project(project)
+    self.backed_projects << project # using @ allows us to directly access that method
+      # this is equivalent to
+      #def backed_projects
+      #  @backed_projects
+      #end
+    project.add_backer(self)
+  end
 
 end # end class
--- lib/project.rb
+++ lib/project.rb
@@ -1,17 +1,15 @@
 class Project
 
-attr_accessor :title, :backers
+  attr_reader :title, :backers
 
-def initialize(title) # takes a title on initialization
-  @title = title # project title
-  @backers = Array.new # initializes empty array of backers which needs to be filled with backers from class Backer
-end
+  def initialize(title) # takes a title on initialization
+    @title = title # project title
+    @backers = Array.new # initializes empty array of backers which needs to be filled with backers from class Backer
+  end
 
-def add_backer(backer)
-
-  @backers << backer
-  backer.backed_projects << self
-  
-end
+  def add_backer(backer)
+    self.backers << backer
+    backer.backed_projects << self # here we are not calling onto the project, but rather creating a procedural command, which helps to avoid a loop
+  end
 
 end # ends class
